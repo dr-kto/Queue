47a43aded8d7cead7c74d0b77355f5594ce7b130
Display None <bakhtrv.dias@gmail.com>
2024-02-10 22:40:27 +0600

    1

--- next.config.js
+++ next.config.js
@@ -1,15 +1,35 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  images: {
-    domains: ['utfs.io'],
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'utfs.io',
-        port: ''
-      }
-    ]
-  }
+    images: {
+        // domains: ['utfs.io'],
+        remotePatterns: [
+            {
+                protocol: 'https',
+                hostname: 'res.cloudinary.com',
+                pathname: '**',
+            },
+            {
+                protocol: 'https',
+                hostname: 'images.unsplash.com',
+                pathname: '**',
+            },
+            {
+                protocol: 'https',
+                hostname: 'avatars.githubusercontent.com',
+                pathname: '**',
+            },
+            {
+                protocol: 'https',
+                hostname: 'lh3.googleusercontent.com',
+                pathname: '**',
+            },
+            {
+                protocol: 'https',
+                hostname: 'utfs.io',
+                port: '',
+            },
+        ],
+    },
 }
 
 module.exports = nextConfig
