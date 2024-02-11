603f1cd49e45664aed85d4309251996c024b9a17
Display None <bakhtrv.dias@gmail.com>
2024-02-12 01:04:27 +0600

    order

--- app/api/webhook/stripe/route.ts
+++ app/api/webhook/stripe/route.ts
@@ -1,6 +1,7 @@
 import stripe from 'stripe'
 import { NextResponse } from 'next/server'
 import { createOrder } from '@/lib/actions/order.actions'
+import { useUser } from '@clerk/nextjs'
 
 export async function POST(request: Request) {
     const body = await request.text()
@@ -20,20 +21,42 @@ export async function POST(request: Request) {
     const eventType = event.type
 
     // CREATE
-    if (eventType === 'checkout.session.completed') {
-        const { id, amount_total, metadata } = event.data.object
+    // if (eventType === 'checkout.session.completed') {
+    //     const { id, amount_total, metadata } = event.data.object
 
-        const order = {
-            stripeId: id,
-            eventId: metadata?.eventId || '',
-            buyerId: metadata?.buyerId || '',
-            totalAmount: amount_total ? (amount_total / 100).toString() : '0',
-            createdAt: new Date(),
-        }
+    //     const order = {
+    //         stripeId: id,
+    //         eventId: metadata?.eventId || '',
+    //         buyerId: metadata?.buyerId || '',
+    //         totalAmount: amount_total ? (amount_total / 100).toString() : '0',
+    //         createdAt: new Date(),
+    //     }
 
-        const newOrder = await createOrder(order)
-        return NextResponse.json({ message: 'OK', order: newOrder })
+    //     const newOrder = await createOrder(order)
+    //     return NextResponse.json({ message: 'OK', order: newOrder })
+    // }
+    const { user } = useUser()
+    const userId = user?.publicMetadata.userId as string
+
+    const { id, amount_total, metadata } = {
+        id: userId,
+        amount_total: 0,
+        metadata: {
+            eventId: event.id,
+            buyerId: userId,
+        },
     }
 
-    return new Response('', { status: 200 })
+    const order = {
+        stripeId: id,
+        eventId: metadata?.eventId || '',
+        buyerId: metadata?.buyerId || '',
+        totalAmount: amount_total ? (amount_total / 100).toString() : '0',
+        createdAt: new Date(),
+    }
+
+    const newOrder = await createOrder(order)
+    return NextResponse.json({ message: 'OK', order: newOrder })
+
+    // return new Response('', { status: 200 })
 }
