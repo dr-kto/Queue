c66157dcea7062c62086a4f68c778eb2e7ca3a84
Display None <bakhtrv.dias@gmail.com>
2024-02-16 02:43:03 +0600

    шаршадым барғой осы Deploymentтан        РЕАЛЬНО (корпек пен жастығымды сағындым)

--- lib/authOptions.ts
+++ lib/authOptions.ts
@@ -70,7 +70,7 @@ export const authOptions: AuthOptions = {
             },
         }),
     ],
-    // debug: process.env.NODE_ENV === 'development',
+    debug: process.env.NODE_ENV === 'development',
     session: {
         strategy: 'jwt',
     },
